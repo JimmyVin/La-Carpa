87461c7a153bf26c3b977e8c3274a5b20239bf6d
jfqvinueza <jfq.vinueza@yavirac.edu.ec>
2025-09-08 20:46:20 -0500

    big bang

---
+++ script.js
@@ -0,0 +1,5 @@
+    // Simulación de carga
+    window.addEventListener("load", () => {
+        document.getElementById("cargando").style.display = "none";
+        document.getElementById("contenido").style.display = "block";
+      });
