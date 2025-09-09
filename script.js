a54e31777c819052da41e1fdc98c5dc0cfd4fde8
jfqvinueza <jfq.vinueza@yavirac.edu.ec>
2025-09-09 09:22:22 -0500

    adicion pagina nueva

--- script.js
+++
@@ -1,5 +0,0 @@
-    // Simulación de carga
-    window.addEventListener("load", () => {
-        document.getElementById("cargando").style.display = "none";
-        document.getElementById("contenido").style.display = "block";
-      });
